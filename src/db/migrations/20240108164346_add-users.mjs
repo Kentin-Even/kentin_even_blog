392abb9724b6427db531f21d10cb2353c900ca3e
Kentin Even <kentineven02gmail.com>
2024-01-14 12:46:40 +0100

    feat: add table users

---
+++ src/db/migrations/20240108164346_add-users.mjs
@@ -0,0 +1,14 @@
+export const up = async (db) => {
+  await db.schema.createTable("users", (table) => {
+    table.increments("id")
+    table.text("email").notNullable().unique()
+    table.enu("role", ["admin, user"]).defaultTo("user").notNullable()
+    table.text("passwordHash").notNullable()
+    table.text("passwordSalt").notNullable()
+    table.timestamps(true, true, true)
+  })
+}
+
+export const down = async (db) => {
+  await db.schema.dropTable("users")
+}
